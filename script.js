5f903242c03ef45c5e8cd8c037e64660cceb668d
lamyt2026 <lamytnik@gmail.com>
2026-07-05 20:28:22 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+const targetDate = new Date('2026-08-22T20:00:00-03:00');
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxf4S2634i4-CdX68U0qKHgS7F15w2fTf2hyjAUHXQkFp_iUOsMpiaEo47DHbhnby9Q/exec';
